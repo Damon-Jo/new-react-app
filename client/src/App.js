563bd5cebe3823f4547bc73ac01f0a16820be435
Damon-Jo <lunaticjky@gmail.com>
2024-03-12 22:26:21 -0700

    connected client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
 import ListHeader from './components/ListHeader';
 
 const App = () => {
+
+const getData = async () =>{
+  const userEmail = 'ania@test.com'
+  try {
+    const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+    console.log(response)
+    const json = await response.json()
+    console.log(json)
+  } catch (err){
+    console.error(err)
+  }
+}
+
+useEffect(() => getData, [])
+
   return (
     <div className='app'>
       <ListHeader listName={'💛Holiday tick list'}/>
--- server/server.js
+++ server/server.js
@@ -1,22 +1,26 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')
 
-
+app.use(cors())
 
 // get all todos
-app.get('/todos', async (req,res)=>{
-
-    try{
-        const todos = await pool.query('SELECT * FROM todos')
-        res.json(todos.rows)
-    }catch (err){
-        console.log(err)
+app.get('/todos/:userEmail', async (req, res) => {
     
+
+    const {userEmail} = req.params
+    console.log(userEmail)
+    try {
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+        res.json(todos.rows)
+    } catch (err) {
+        console.log(err)
+
     }
 
 })
 
 
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
