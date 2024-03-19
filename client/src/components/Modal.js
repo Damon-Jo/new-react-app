ba1eda4d885f1850237860a9a96a672311091e97
Damon-Jo <lunaticjky@gmail.com>
2024-03-18 22:43:32 -0700

    created modal for adding function

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,46 @@
 const Modal = () => {
-    return (
-      <div>
-        
+
+  const mode = "edit";
+  const handleChange = () => {
+    console.log('changing')
+  };
+
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} you task</h3>
+          <button>X</button>
+        </div>
+
+        <form>
+          <input
+            required
+            maxLength={30}
+            placeholder=" Your task goes here"
+            name="title"
+            value={""}
+            onChange={handleChange}
+          />
+          <br />
+          <label for="range">Drag to select your current progress</label>
+          <input
+            required
+            type="range"
+            id="range"
+            min='0'
+            max="100"
+            name="progress"
+            value={""}
+            onAbort={handleChange}
+
+          />
+          <input className={mode} type="submit" />
+        </form>
       </div>
-    );
-  }
-  
-  export default Modal;
-  
+
+    </div>
+  );
+}
+
+export default Modal;
