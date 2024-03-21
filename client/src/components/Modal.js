c95f5784caffb8866878913020a6d4f78e44c502
Damon-Jo <lunaticjky@gmail.com>
2024-03-20 21:22:33 -0700

    functionalized modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
-const Modal = () => {
+const Modal = ({mode, setShowModal}) => {
 
-  const mode = "create"
+
   const editMode = mode === 'edit' ? true : false
 
   const [data, setData] =  useState({
@@ -16,21 +16,23 @@ const Modal = () => {
 
 
   const handleChange = (e) => {
-    console.log('changing', e)
+    // console.log('changing', e)
     const { name, value } = e.target;
 
     setData(data => ({
       ...data,
       [name]: value
     }))
+
+    console.log(data)
   }
 
   return (
     <div className="overlay">
       <div className="modal">
         <div className="form-title-container">
-          <h3>Let's {mode} you task</h3>
-          <button>X</button>
+          <h3>Let's {mode} your task</h3>
+          <button onClick={()=> setShowModal(false)}>X</button>
         </div>
 
         <form>
@@ -52,7 +54,7 @@ const Modal = () => {
             max="100"
             name="progress"
             value={data.progress}
-            onChange={handleChange}
+            onChange={handleChange}   
 
           />
           <input className={mode} type="submit" />
