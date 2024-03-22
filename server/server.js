2c73991d308b25ef7f4d44319aa2228ef576f515
Damon-Jo <lunaticjky@gmail.com>
2024-03-21 21:31:54 -0700

    added edit api to edit the todo with db

--- server/server.js
+++ server/server.js
@@ -34,8 +34,20 @@ app.post('/todos', async (req, res) => {
     } catch (err) {
         console.log(err)
     }
-}
-)
+})
+
+// create a todo
+app.put('/todos/:id', async (req, res) => {
+    const { id } = req.params
+    const { user_email, title, progress, date } = req.body
+    try {
+        const editToDo = await pool.query('UPDATE todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;',
+            [user_email, title, progress, date, id])
+        res.json(editToDo)
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
