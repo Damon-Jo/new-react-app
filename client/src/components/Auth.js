d74a1b1cdf6e42b18933a95d5285e98ce1a10d60
Damon-Jo <lunaticjky@gmail.com>
2024-03-25 22:14:16 -0700

    created Auth component for login function

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,10 +1,38 @@
+import { useState } from "react"
+
 const Auth = () => {
-    return (
-      <div>
-        
-      </div>
-    );
+  const [isLogIn, setIsLogIn] = useState(true)
+  const [error, setError] = useState(null)
+
+  const viewLogin = (status) => {
+    setError(null)
+    setIsLogIn(status)
   }
-  
-  export default Auth;
-  
+
+  return (
+    <div className="auth-container">
+      <div className="auth-container-box">
+        <form>
+          <h2>{isLogIn ? 'Please log in' : 'Please sign up!'}</h2>
+          <input type='email' placeholder="email" />
+          <input type='password' placeholder="password" />
+          {!isLogIn && <input type='password' placeholder="confirm password" />}
+          <input type='submit' className="create" />
+          {error && <p className="error">{error}</p>}
+        </form>
+        <div className="auth-options">
+          <button 
+          onClick={()=> viewLogin(false)}
+          style={{backgroundColor: isLogIn ? 'rgb(255,255,255)' : 'rgb(188,188, 188)'}}
+          >Sign up</button>
+          <button 
+          onClick={()=> viewLogin(true)}
+
+          >Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
